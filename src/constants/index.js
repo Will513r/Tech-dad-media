b796e0f41df59449f3cd8d86eeb3c06c768a5dc3
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-07-14 15:01:44 +0530

    statistics section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -88,18 +88,18 @@ export const feedback = [
 export const stats = [
   {
     id: "stats-1",
-    title: "User Active",
-    value: "3800+",
+    title: "Trusted Clients",
+    value: "15+",
   },
   {
     id: "stats-2",
-    title: "Trusted by Company",
-    value: "230+",
+    title: "Projects Completed",
+    value: "23+",
   },
   {
     id: "stats-3",
-    title: "Transaction",
-    value: "$230M+",
+    title: "Total Transactions",
+    value: "$6500+",
   },
 ];
 
